a55c70ada8d3d43d0915d944b05eda8049e0e648
DamnBoy420 <sander.henricsson@elev.ga.lbs.se>
2025-12-01 11:22:11 +0100

    Done with level 3 design

---
+++ Platformsspel/Assets/Scripts/BlockGame/PointSystem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BlockMovement : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+        }
+    }
+}
