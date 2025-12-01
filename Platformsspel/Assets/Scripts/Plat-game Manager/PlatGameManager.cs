e044a9bd2f9d029a5b9b0c3c07b5fd3777d5ca70
DamnBoy420 <sander.henricsson@elev.ga.lbs.se>
2025-12-01 13:25:50 +0100

    The beginning of PointSpawner

---
+++ Platformsspel/Assets/Scripts/Plat-game Manager/PlatGameManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlatGameManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject pointPrefab;
+
+    Vector2 spawnPos;
+
+    public void PointSpawner()
+    {
+        (-13.25, 5.75)
+    }
+}
